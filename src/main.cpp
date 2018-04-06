e60d35448f91dfe95e29b0a1ba7a30b347d2a009
acherep <acherep@mail.ru>
2018-04-06 22:08:44 +0200

    Texture, Renderer and Buffer added

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
   const int SCREEN_WIDTH = 800;
   const int SCREEN_HEIGHT = 600;
-  cout << "Hello World13332342342" << endl;
+  const int WHITE_COLOR = 0xff;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     cout << "SDL init failed" << endl;
@@ -21,6 +21,36 @@ int main() {
     SDL_Quit();
     return 2;
   }
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  if (renderer == NULL) {
+    cout << "Could not create renderer!" << endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 3;
+  }
+
+  SDL_Texture *texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+  if (texture == NULL) {
+    cout << "Could not create texture!" << endl;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 4;
+  }
+
+  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+  memset(buffer, WHITE_COLOR, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
   bool quit = false;
   SDL_Event event;
   while (!quit) {
@@ -30,6 +60,10 @@ int main() {
       }
     }
   }
+
+  delete[] buffer;
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
 
   SDL_DestroyWindow(window);
   SDL_Quit();
