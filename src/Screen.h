f3eadc7380114a2a68400d900290eca07af4a3f2
acherep <acherep@mail.ru>
2018-04-08 20:53:27 +0200

    Screen class introduced

---
+++ src/Screen.h
@@ -0,0 +1,32 @@
+#ifndef SCREEN_H_
+
+#define SCREEN_H_
+
+#include <SDL2/SDL.h>
+
+namespace acherep {
+
+class Screen {
+ public:
+  static const int SCREEN_WIDTH = 800;
+  static const int SCREEN_HEIGHT = 600;
+  static const int SCREEN_COLOR = 0;
+
+ private:
+  SDL_Window *m_window;
+  SDL_Renderer *m_renderer;
+  SDL_Texture *m_texture;
+  Uint32 *m_buffer;
+
+ public:
+  Screen();
+  bool init();
+  void update();
+  void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
+  bool processEvents();
+  void close();
+};
+
+}  // namespace acherep
+
+#endif  // SCREEN_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,33 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include "Screen.h"
 
 using namespace std;
+using namespace acherep;
 
 int main() {
-  const int SCREEN_WIDTH = 800;
-  const int SCREEN_HEIGHT = 600;
-  const int WHITE_COLOR = 0xff;
+  Screen screen;
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    cout << "SDL init failed" << endl;
+  if (screen.init() == false) {
+    cout << "Error initializing SDL" << endl;
     return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow(
-      "Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED,
-      SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-  if (window == NULL) {
-    SDL_Quit();
-    return 2;
-  }
-
-  SDL_Renderer *renderer =
-      SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-  if (renderer == NULL) {
-    cout << "Could not create renderer!" << endl;
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 3;
-  }
-
-  SDL_Texture *texture =
-      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
-  if (texture == NULL) {
-    cout << "Could not create texture!" << endl;
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 4;
-  }
-
-  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
-
-  memset(buffer, WHITE_COLOR, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
-  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
-  SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, texture, NULL, NULL);
-  SDL_RenderPresent(renderer);
-
-  bool quit = false;
-  SDL_Event event;
-  while (!quit) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        quit = true;
+  while (true) {
+    for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
+      for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
+        screen.setPixel(x, y, 128, 128, 0);
       }
+    }
+
+    screen.setPixel(400, 300, 255, 255, 255);
+    screen.update();
+    if (screen.processEvents() == false) {
+      break;
     }
   }
 
-  delete[] buffer;
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  screen.close();
 
   return 0;
 }
