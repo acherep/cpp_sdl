05bce5d45bc62f696b3f45e5145be875d71b4c7b
acherep <acherep@mail.ru>
2018-04-11 10:41:41 +0200

    Adding rotation and reinitialization of the particles

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -3,13 +3,30 @@
 #include <stdlib.h>
 
 namespace acherep {
-Particle::Particle() : m_x(0), m_y(0) {
+Particle::Particle() { init(); }
+
+void Particle::init() {
+  m_x = 0;
+  m_y = 0;
   m_direction = 2 * M_PI * rand() / RAND_MAX;
   m_speed = MAX_SPEED * rand() / RAND_MAX;
+
+  m_speed = m_speed * m_speed;
 }
 
 void Particle::update(int timeInterval) {
+  // rotation
+  m_direction += timeInterval * 0.0003;
+
   m_x += m_speed * cos(m_direction) * timeInterval;
   m_y += m_speed * sin(m_direction) * timeInterval;
+  // reinitialize the particle when it runs out of the edge of the screen
+  if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1) {
+    init();
+  }
+  // randomly reinitialize the particle
+  if (rand() < RAND_MAX / 100) {
+    init();
+  }
 }
 }  // namespace acherep
--- src/Particle.h
+++ src/Particle.h
@@ -3,13 +3,15 @@
 
 namespace acherep {
 struct Particle {
-  const static double MAX_SPEED = 0.0001;
-  double m_x;
-  double m_y;
-
+ private:
   double m_speed;
   double m_direction;
+  void init();
 
+ public:
+  const static double MAX_SPEED = 0.04;
+  double m_x;
+  double m_y;
   Particle();
   void update(int timeInterval);
 };
